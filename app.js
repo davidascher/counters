39afb00f20f5ba52972c48f09c00f077e6fc1d2f
travis-ci <travis@travis-ci.org>
2014-06-15 17:44:56 -0700

    add logging

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var app = express();
 var redis = require("redis").createClient();
 
 if (process.env.REDISTOGO_URL) {
+  console.log("FOUND REDISTOGO:", process.env.REDISTOGO_URL);
   var rtg   = require("url").parse(process.env.REDISTOGO_URL);
   var redis = require("redis").createClient(rtg.port, rtg.hostname);
 
   redis.auth(rtg.auth.split(":")[1]);
 } else {
+  console.log("didnt' find REDISTOGOURL");
   var redis = require("redis").createClient();
 }
 
