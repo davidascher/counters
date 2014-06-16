023cef4cf60de10c52a8470552cf99b82aa05198
travis-ci <travis@travis-ci.org>
2014-06-15 18:22:48 -0700

    add JSON endpoint for actual use in a website counter

--- app.js
+++ app.js
@@ -21,10 +21,16 @@ app.get('/', function(req, res) {
   res.send("<link rel='stylesheet' type='text/css' href='main.css' /><h1>Countzilla</h1><p>This website counts.  We're counting <a href='/foo'>foos</a> for example.</p>");
 });
 
-  app.get('/:name', function(req, res) {
+app.get('/:name', function(req, res) {
   redis.get('counter-'+req.params.name, function(err, result) {
     if (result == null) result = 0;
-    res.send("<link rel='stylesheet' type='text/css' href='main.css' /><h1>Counting: " + result + ' ' + req.params.name +'s' + '</h1><p>Counter '+ req.params.name + ' was hit <span class="counter">' + result + '</span> times. <a href="">Reloading this page</a> won\'t change that.  <a href="/add/' + req.params.name +'">hit me to increment</a>');
+    res.send("<link rel='stylesheet' type='text/css' href='main.css' /><h1>Counting: " + result + ' ' + req.params.name +'s' + '</h1><p>Counter '+ req.params.name + ' was hit <span class="counter">' + result + '</span> times. <a href="">Reloading this page</a> won\'t change that.  <a href="/add/' + req.params.name +'">hit me to increment</a>.  The <a href="/raw/' + req.params.name + '">API version</a>');
+  });
+});
+app.get('/raw/:name', function(req, res) {
+  redis.get('counter-'+req.params.name, function(err, result) {
+    if (result == null) result = 0;
+    res.json({'counter': req.params.name, 'value': result});
   });
 });
 
