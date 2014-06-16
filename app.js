a4561daee4532c75b38f5e6cc7cfe3287ef5a25c
travis-ci <travis@travis-ci.org>
2014-06-15 17:36:34 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+var redis = require("redis").createClient();
+
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+
+  redis.auth(rtg.auth.split(":")[1]);
+} else {
+  var redis = require("redis").createClient();
+}
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  redis.get('counter', function(err, result) {
+    res.send('Counter was hit ' + result + ' times');
+  });
+});
+
+app.get('/count', function(req, res) {
+  redis.incr('counter', function(err, result) {
+    res.send('Counter incremented to' + result);
+  })
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
